49c9d8785e0a2e39bcc18453396f20f335080b90
nickvirtue <nick@mahuika.co.nz>
2022-06-29 15:43:52 +1200

    Test title is shown

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders App Title', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = await screen.findByText("First AML Horse");
   expect(linkElement).toBeInTheDocument();
 });
+
